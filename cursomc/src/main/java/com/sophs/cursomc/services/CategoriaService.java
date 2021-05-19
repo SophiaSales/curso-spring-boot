61c3c4f6775de5527c749091fb7490b6d91740e6
SophiaSales <sophiabrenda10@gmail.com>
2021-05-19 17:14:34 -0300

    endpoint aula20

--- cursomc/src/main/java/com/sophs/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/sophs/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.sophs.cursomc.domain.Categoria;
 import com.sophs.cursomc.repositories.CategoriaRepository;
+import com.sophs.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
