3b7585bb3e61608863c69523f82691cd019177c6
SophiaSales <sophiabrenda10@gmail.com>
2021-05-20 14:34:39 -0300

    Endpoint aula30

---
+++ cursomc/src/main/java/com/sophs/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.sophs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sophs.cursomc.domain.Pedido;
+import com.sophs.cursomc.repositories.PedidoRepository;
+import com.sophs.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
