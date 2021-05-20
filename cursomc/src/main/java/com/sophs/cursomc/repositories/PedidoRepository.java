6185b58fe8a633c094d56ac65db0216d9f4d7eed
SophiaSales <sophiabrenda10@gmail.com>
2021-05-20 11:32:07 -0300

    Pedido estadoPagamento e pagamento aula27

---
+++ cursomc/src/main/java/com/sophs/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.sophs.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sophs.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
