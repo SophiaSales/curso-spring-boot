d9081b895eb516e4ea7f7508da032516b6478496
SophiaSales <sophiabrenda10@gmail.com>
2021-05-21 21:39:49 -0300

    Inserindo pedido aula53

---
+++ cursomc/src/main/java/com/sophs/cursomc/services/BoletoService.java
@@ -0,0 +1,20 @@
+package com.sophs.cursomc.services;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import org.springframework.stereotype.Service;
+
+import com.sophs.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
