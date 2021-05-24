e2301ef90919da0d05ada83a7592004501a681a5
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 19:58:21 -0300

    Endpoints estados e cidades aula97

---
+++ cursomc/src/main/java/com/sophs/cursomc/services/CidadeService.java
@@ -0,0 +1,20 @@
+package com.sophs.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sophs.cursomc.domain.Cidade;
+import com.sophs.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+
+	public List<Cidade> findByEstado(Integer estadoId) {
+		return repo.findCidades(estadoId);
+	}
+}
