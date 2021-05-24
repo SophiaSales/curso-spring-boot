5297e0574f24a86884621026d92cb9be99e59d43
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 08:42:22 -0300

    SmtpEmailService aula64

--- cursomc/src/main/java/com/sophs/cursomc/config/DevConfig.java
+++ cursomc/src/main/java/com/sophs/cursomc/config/DevConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.sophs.cursomc.services.DBService;
+import com.sophs.cursomc.services.EmailService;
+import com.sophs.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,9 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
