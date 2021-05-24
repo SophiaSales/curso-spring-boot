4a3cd7aee6d91f9f658591d71511637c8a462573
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 16:16:49 -0300

    Esqueceu a senha aula78

--- cursomc/src/main/java/com/sophs/cursomc/services/EmailService.java
+++ cursomc/src/main/java/com/sophs/cursomc/services/EmailService.java
@@ -1,9 +1,8 @@
 package com.sophs.cursomc.services;
 
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.mail.SimpleMailMessage;
 
+import com.sophs.cursomc.domain.Cliente;
 import com.sophs.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -11,8 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
-	
-	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
-	void sendHtmlEmail(MimeMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
