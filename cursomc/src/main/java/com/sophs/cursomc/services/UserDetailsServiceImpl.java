7a006eedcc7ffa04745d0dc33aea58b973a6abae
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 14:22:51 -0300

    Altenticaçao aula72

---
+++ cursomc/src/main/java/com/sophs/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sophs.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sophs.cursomc.domain.Cliente;
+import com.sophs.cursomc.repositories.ClienteRepository;
+import com.sophs.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
