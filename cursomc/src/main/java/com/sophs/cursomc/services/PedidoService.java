2509870d897305095d0f8a6a05e8ba02ebaab2b6
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 15:40:58 -0300

    Restrição de conteúdo aula76

--- cursomc/src/main/java/com/sophs/cursomc/services/PedidoService.java
+++ cursomc/src/main/java/com/sophs/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.sql.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sophs.cursomc.domain.Cliente;
 import com.sophs.cursomc.domain.ItemPedido;
 import com.sophs.cursomc.domain.PagamentoComBoleto;
 import com.sophs.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.sophs.cursomc.domain.enums.EstadoPagamento;
 import com.sophs.cursomc.repositories.ItemPedidoRepository;
 import com.sophs.cursomc.repositories.PagamentoRepository;
 import com.sophs.cursomc.repositories.PedidoRepository;
+import com.sophs.cursomc.security.UserSS;
+import com.sophs.cursomc.services.exceptions.AuthorizationException;
 import com.sophs.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -62,5 +68,15 @@ public class PedidoService {
 	    itemPedidoRepository.saveAll(obj.getItens());
 	    emailService.sendOrderConfirmationHtmlEmail(obj);
 	    return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
