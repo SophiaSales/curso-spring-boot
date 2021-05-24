c3ea15b8fc7772b1dc394b89c3c184a7a279f46d
SophiaSales <sophiabrenda10@gmail.com>
2021-05-24 17:58:00 -0300

    Salvando primeiro arquivo aula84

--- cursomc/src/main/java/com/sophs/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/sophs/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.sophs.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
+	@Autowired
+//	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+//		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
 		
 	}
 }
